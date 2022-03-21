826e09ec37221240b6889954162a12bf080b6882
Denis Ivanov <gramoturgip@gmail.com>
2022-03-21 15:44:59 -0700

    React App and list created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+const todoList = [
+  {
+    title: "Complete Task 1",
+    id: 1
+  },
+  {
+    title: "Complete Task 2",
+    id: 2
+  },
+  {
+    title: "Complete Task 3",
+    id: 3
+  }
+];
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map(function (item) {
+          return (<li key={item.id}>{item.title}</li>)
+        })}
+      </ul>
     </div>
   );
 }
