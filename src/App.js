2063ad2fadac32cb5b23b02b118cd8fec53382b6
Denis Ivanov <gramoturgip@gmail.com>
2022-04-12 14:44:06 -0700

    lesson 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
+
+  const [newTodo, setNewTodo] = useState('');
+
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <AddTodoForm/>
+      <AddTodoForm onAddTodo={setNewTodo}/>
+      <p>{newTodo}</p>
       <TodoList/>
     </div>
   );
